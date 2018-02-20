53a30432b9c051d03c31a1a356a566bafdd24e83
knoacc <knoacc@users.noreply.github.com>
2018-02-21 02:03:13 +0700

    Update safelink-mainv2.js

--- javascripts/safelink-mainv2.js
+++ javascripts/safelink-mainv2.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="2017/03/berwisata-gratis-di-wilayah-malang-raya.html"}
 if(x=="2"){xxx="2017/03/10-lokasi-penyelaman-dengan-view.html"}
 if(x=="3"){xxx="2017/03/25-tujuan-wisata-di-jogja-instagamable.html"}
 if(x=="4"){xxx="2017/03/10-tips-for-start-small-business-startup.html"}
-if(x=="4"){xxx="2018/02/jenis-jenis-asuransi-jiwa-yang-mesti-anda-ketahui.html"}
+if(x=="5"){xxx="2018/02/jenis-jenis-asuransi-jiwa-yang-mesti-anda-ketahui.html"}
 a_to_ve[i].href="https://safelink.knoacc.org/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
